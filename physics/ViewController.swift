639960bf369f1391d92c57b7e129a6c3b98980c5
period3 <period3@p-162mba-02.local>
2019-04-08 10:09:11 -0500

    Initial Commit

---
+++ physics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  physics
+//
+//  Created by period3 on 4/8/19.
+//  Copyright © 2019 period3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
